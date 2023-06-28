d591eb72cfc5516e5069550a2501445e63d1223e
dangvanthua <thuandang021102@gmail.com>
2023-06-28 21:25:34 +0700

    Set API for template post
    - Create file home.js to alternative for file main.js
    - Create file common.js to use for same works
    - Create file index.js to import

---
+++ js/utils/common.js
@@ -0,0 +1,8 @@
+export function setTextContent(parent, selector, text) {
+    if (!parent) return
+
+    const element = parent.querySelector(selector)
+    if (element) {
+        element.textContent = text
+    }
+}
